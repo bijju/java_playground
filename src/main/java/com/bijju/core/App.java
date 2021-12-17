a334e1bb38893d7bcfabf01eaf3b13577525a48c
Bijju Bhattaram <bijjubhattaram@gmail.com>
2021-12-16 19:16:39 -0800

    Working Log4j2 Code

--- src/main/java/com/bijju/core/App.java
+++ src/main/java/com/bijju/core/App.java
@@ -1,11 +1,18 @@
 package com.bijju.core;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Properties;
 
-public class App {
-    public static void main(String[] args) {
-        Properties props = new Properties();
-        props.load(App.class.getClass().getResourceAsStream("log4j.properties"));
-        PropertyConfigurator.configure(props);
+public class App
+{
+    private static final Logger log = LogManager.getLogger(App.class);
+
+    public static void main(String[] args)
+    {
+        //PropertiesConfigurator is used to configure logger from properties file
+        log.info("Starting app: .....");
+        log.info("Shutting Down: Done!");
     }
 }
